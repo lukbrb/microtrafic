6ef151919a7b429963e2fd28a10a22deb9f8da63
lukbrb <lucasbarbr@outlook.com>
2023-08-27 15:26:19 +0200

    ajout generation immatriculation et couleur voiture

--- microtrafic/microtrafic.py
+++ microtrafic/microtrafic.py
@@ -1,7 +1,9 @@
-import itertools
 import logging
+import random
+import string
 from typing import List
 
+from matplotlib.colors import CSS4_COLORS
 
 dt = 0.1
 TEMPS_SECUR = 1.
@@ -30,19 +32,41 @@ file_handler.setFormatter(file_formatter)
 logging.getLogger('').addHandler(file_handler)
 
 
+def generate_random_immatriculation() -> str:
+    letters1 = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase)
+    numbers = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+    letters2 = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase)
+
+    return letters1 + '-' + numbers + '-' + letters2
+
+
+def generate_random_color() -> str:
+    return random.choice(list(CSS4_COLORS.keys()))
+
+
 class Voiture:
-    def __init__(self, x, y, v, nom="baniol", a=0) -> None:
+    def __init__(self, x, y, v, nom=None, couleur=None, a=0) -> None:
         self.x = x
         self.y = y
         self.v = v
         self.a = a
         self.nom = nom
+        self.couleur = couleur
+        self.set_color()
+        self.set_name()
 
     def __str__(self) -> str:
-        return f"{self.nom.title()}(x={self.x:.2f}, y={self.y:.2f}, v={self.v:.2f})"
+        return f"{self.nom}(x={self.x:.2f}, y={self.y:.2f}, v={self.v:.2f})"
     
     def __repr__(self) -> str:
-        return f"{self.nom.title()}(x={self.x:.2f}, y={self.y:.2f}, v={self.v:.2f})"
+        return f"{self.nom}(x={self.x:.2f}, y={self.y:.2f}, v={self.v:.2f})"
+    
+    def set_color(self) -> None:
+        self.couleur = generate_random_color() if self.couleur is None else self.couleur
+
+    def set_name(self) -> None:
+        self.nom = generate_random_immatriculation() if self.nom is None else self.nom
+
     def avance(self) -> None:
         dy = 0.5 * self.a * dt**2 + self.v * dt
         self.y += dy
@@ -80,25 +104,29 @@ class Route:
         distance_secur = voiture.v * temps_secur
         for other in self.voitures:
             if x == other.x and voiture != other:  # Véhicules sur la même ligne, et pas lui-même
-                if abs(y - other.y) < distance_secur:
+                if abs(y - other.y % self.distance) < distance_secur:
+                    print(f"{voiture} trop proche de {other}")
                     return False
         return True
     
-    # def distance_securite_avancer(self, x: float, y: float, voiture: Voiture, temps_secur: float = 1.) -> bool:
-    #     distance_secur = voiture.v * temps_secur
-    #     for other in self.voitures:
-    #         if x == other.x and voiture != other:  # Véhicules sur la même ligne, et pas lui-même
-    #             d = y - other.y
-    #             if d > 0 and abs(d) < distance_secur:  # on veut s'assurer que la voiture est devant
-    #                 return False
-    #     return True
-    
+    def distance_securite_avant(self, voiture, temps_secur):
+        distance_secur = voiture.v * temps_secur
+        for other in self.voitures:
+            if other.x != voiture.x:
+                return True
+            elif other.x < voiture.x:
+                return True
+            elif abs(other.x - voiture.x) > distance_secur:
+                return True
+            else:
+                return False
+            
     def step(self) -> None:
         for voiture in self.voitures:
             if self.distance_securite(voiture.x + sens_changement['droite'], voiture.y + voiture.v * dt, voiture) and voiture.x < 0:
                 voiture.rabattement()
                 action = 'Rabattement'
-            elif self.distance_securite(voiture.x, voiture.y + voiture.v * dt, voiture):
+            elif self.distance_securite_avant(voiture, TEMPS_SECUR):
                 voiture.avance()
                 action = 'Avance'
             elif self.distance_securite(voiture.x + sens_changement['gauche'], voiture.y + voiture.v * dt, voiture) and (voiture.x - 1) > -NVOIES:
@@ -109,5 +137,5 @@ class Route:
                 action = 'Attente'
 
             voiture.y %= self.distance
-            logging.info(f"Étape : {self.pas} - Position ({voiture.x:.2f}, {voiture.y:.2f}) - Action: {action}")
+            logging.info(f"Étape : {self.pas} - {voiture.nom}- Position ({voiture.x:.2f}, {voiture.y:.2f}) - Action: {action}")
             
