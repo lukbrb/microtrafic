d9320adf67dc6075df3d3b90749d58c7b2dd8824
lukbrb <lucasbarbr@outlook.com>
2023-09-01 17:48:16 +0200

    changement paramètres et création Bornes

--- microtrafic/read_json.py
+++ microtrafic/read_json.py
@@ -6,16 +6,33 @@ from dataclasses import dataclass
 class Parameters:
     NVOIES: int
     ROAD_WIDTH: float
-    VMAX: int
+    ROAD_LEN: int
+    VMAX: float
+    VMIN: float
     NVOITURES: int
     DT: float
     TEMPS_SECUR: float
-    bornes_y: tuple
-    bornes_v: tuple
     sens_changement: dict
 
+@dataclass
+class Bornes:
+    x: tuple
+    y: tuple
+    v: tuple
 
-def read_params(file_name):
+
+def read_params(file_name: str = 'microtrafic/parameters.json'):
     with open(file_name, "r") as json_file:
         return Parameters(**json.load(json_file))
-        
+
+
+def read_bornes(parameters: Parameters) -> Bornes:
+    bornes_x = (-parameters.NVOIES + 1, 0)
+    bornes_y = (0, parameters.ROAD_LEN - 1)
+    bornes_v = (parameters.VMIN, 1)
+    return Bornes(x=bornes_x, y=bornes_y, v=bornes_v)
+
+
+# bornes_x = (-NVOIES + 1, 0)
+# bornes_y = (0, 99)
+# bornes_v = (0.7, 1)
