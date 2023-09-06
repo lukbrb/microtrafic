ae7680bcf4e0334d252530c84627499ee13b432a
lukbrb <lucasbarbr@outlook.com>
2023-09-06 21:53:03 +0200

    mise en forme

--- microtrafic/run.py
+++ microtrafic/run.py
@@ -10,31 +10,38 @@ from microtrafic import Voiture, Route
 params = read_params()
 bornes = read_bornes(params)
 
-voitures = [Voiture(random.randint(*bornes.x), random.uniform(*bornes.y), random.uniform(*bornes.v) * params.VMAX) for _ in range(params.NVOITURES)]
+voitures = [Voiture(random.randint(*bornes.x), random.uniform(*bornes.y), random.uniform(*bornes.v) * params.VMAX) for _
+            in range(params.NVOITURES)]
 
 route = Route(voitures, nvoies=params.NVOIES, distance=100)
 
 fig, ax = plt.subplots()
 ax.set_xlim(-params.NVOIES, params.ROAD_WIDTH)
 ax.set_ylim(0, route.distance)
-#ax.set_axis_off()
+# ax.set_axis_off()
 
-#ax.axis('off')
+# ax.axis('off')
 # Initialisation des points
-points = [(ax.plot(voiture.x, voiture.y, color=voiture.couleur, marker='.', markersize=10))[0] for i, voiture in enumerate(voitures)]
-#etape = [ax.plot(0, 0, '.k', label=f'Étape = {route.pas}')[0]]
+points = [(ax.plot(voiture.x, voiture.y, color=voiture.couleur, marker='.', markersize=10))[0] for i, voiture in
+          enumerate(voitures)]
+
+
+# etape = [ax.plot(0, 0, '.k', label=f'Étape = {route.pas}')[0]]
 # Création de la route 
 
 
-def genere_route(ax: plt.Axes, route: Route, nvoies: int, rwidth: float) -> List[LineCollection]:
+def genere_route(axe: plt.Axes, road: Route, nvoies: int, rwidth: float) -> List[LineCollection]:
     multiplicateurs = genere_impaires(nvoies)
-    ligne_droite = [ax.vlines(rwidth, 0, route.distance, color='w', linestyles='-')]
-    interlignes = [ax.vlines(-i * params.ROAD_WIDTH, 0, route.distance, color='w', linestyles='--') for i in multiplicateurs[:-1]]  # La dernière ligne doit être solide
-    ligne_gauche = [ax.vlines(-multiplicateurs[-1] * params.ROAD_WIDTH, 0, route.distance, color='w', linestyles='-')]
+    ligne_droite = [axe.vlines(rwidth, 0, road.distance, color='w', linestyles='-')]
+    interlignes = [axe.vlines(-i * params.ROAD_WIDTH, 0, road.distance, color='w', linestyles='--') for i in
+                   multiplicateurs[:-1]]  # La dernière ligne doit être solide
+    ligne_gauche = [axe.vlines(-multiplicateurs[-1] * params.ROAD_WIDTH, 0, road.distance, color='w', linestyles='-')]
     return ligne_droite + interlignes + ligne_gauche
 
+
 ax.set_facecolor('gray')
-lignes = genere_route(ax=ax, route=route, nvoies=params.NVOIES, rwidth=params.ROAD_WIDTH)
+lignes = genere_route(axe=ax, road=route, nvoies=params.NVOIES, rwidth=params.ROAD_WIDTH)
+
 
 def update(frame):
     # Mettre à jour les coordonnées des points (simulation du mouvement)
@@ -42,11 +49,12 @@ def update(frame):
     route.step()
 
     for i in range(len(points)):
-    # Mettre à jour les données des points dans les graphiques
+        # Mettre à jour les données des points dans les graphiques
         points[i].set_data(voitures[i].x, voitures[i].y % route.distance)
-        #points[i].set_label(f'{voitures[i].nom} (y = {voitures[i].y: .2f})')
-    #etape[0].set_label(f'Étape = {route.pas}')
+        # points[i].set_label(f'{voitures[i].nom} (y = {voitures[i].y: .2f})')
+    # etape[0].set_label(f'Étape = {route.pas}')
     return points
+
 
 # Durée totale de la simulation en secondes
 total_duration = 3
@@ -54,8 +62,8 @@ total_duration = 3
 fps = 30
 total_frames = int(total_duration * fps)
 # Créer l'animation en appelant la fonction update à chaque image (frame)
-ani = FuncAnimation(fig,update, frames=range(total_frames), interval=1000/fps, blit=True)
-#ax.legend()
+ani = FuncAnimation(fig, update, frames=total_frames, interval=1000 / fps, blit=True)
+# ax.legend()
 
 plt.tight_layout()  # Ajuster la disposition des éléments de la figure
 plt.show()
