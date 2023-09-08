603b363a4fbf8e694fa8ade97bb1da44d349bf34
lukbrb <lucasbarbr@outlook.com>
2023-09-08 16:46:35 +0200

    début fonction generate_cars

--- microtrafic/utils.py
+++ microtrafic/utils.py
@@ -37,6 +37,15 @@ def read_bornes(parameters: Parameters) -> Bornes:
     return Bornes(x=bornes_x, y=bornes_y, v=bornes_v)
 
 
+def check_distance(voiture: Voiture, l_voitures: List[Voiture], temps_secur: float) -> bool:
+    distance_secur = voiture.v * temps_secur
+    for other in l_voitures:
+        for other in l_voitures:
+            if voiture.x == other.x and voiture != other:  # Véhicules sur la même ligne, et pas lui-même
+                if abs(voiture.y - other.y) < distance_secur:
+                    return False
+    return True
+
 def genere_impaires(n: int) -> List[int]:
     """ Pour 2 voies on veut [-1, -3] * road_width
              3 -> [-1, -3, -5]
@@ -58,14 +67,22 @@ def generate_random_color() -> str:
     return random.choice(list(CSS4_COLORS.keys()))
 
 
-def generate_cars(nvoitures: int, bornes: Bornes, vmax: float) -> List[Voiture]:
+def generate_cars(nvoitures: int, bornes: Bornes, vmax: float, temps_secur: float) -> List[Voiture]:
     """ Généère une liste de voitures en s'assurant qu'elles 
         occupent toutes une position autorisée.
     """
     if not isinstance(bornes, Bornes):
         raise TypeError(f"bornes est de type {type(bornes)}, mais doit être de type Bornes.")
     
-    liste_basique = [Voiture(random.randint(*bornes.x), random.uniform(*bornes.y), random.uniform(*bornes.v) * vmax) for _ in range(nvoitures)]
+    # liste_basique = [Voiture(random.randint(*bornes.x), random.uniform(*bornes.y), random.uniform(*bornes.v) * vmax) for _ in range(nvoitures)]
+    liste_voitures = []
+
+    for i in range(nvoitures):
+        can_be_added = True
+        voiture = Voiture(random.randint(*bornes.x), random.uniform(*bornes.y), random.uniform(*bornes.v) * vmax)
+        if check_distance(voiture, liste_voitures):
+            liste_voitures.append(voiture)
+
 
     # Il faut maintenant s'assurer que toutes les distances de sécurité soient respectées pour toutes les voitures, et sinon, changer la voiture à un autre endroit valide.
     # A priori, les insérer une par une pour procéder à une vérif semble le plus simple. 
